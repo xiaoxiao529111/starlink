089c26a0ddb062290cedefbd426855607768b1ce
xiaoxiao529111 <xil084@ucsd.edu>
2021-11-03 11:27:31 -0700

    Update main.js and main.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,13 @@ import { Row, Col } from 'antd';
 class Main extends Component {
     render() {
         return (
-            <Row>
-                main
+            <Row className='Main'>
+                <Col span={8} className="left-side">
+                    left 
+                </Col>
+                <Col span={16} className="right-side">
+                    right
+                </Col>
             </Row>
         );
     }
