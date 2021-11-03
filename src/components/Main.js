25b6e948a603586eed4355b6da36656840c8fca8
xiaoxiao529111 <xil084@ucsd.edu>
2021-11-03 11:17:30 -0700

    Create Main.js and Main.css

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Row, Col } from 'antd';
+
+class Main extends Component {
+    render() {
+        return (
+            <Row>
+                main
+            </Row>
+        );
+    }
+}
+export default Main;
