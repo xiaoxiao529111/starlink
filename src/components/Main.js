bb393ec3e8586589940fd71ed8ee6f39294511ad
xiaoxiao529111 <xil084@ucsd.edu>
2021-11-03 18:59:34 -0700

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,58 @@
+
 import React, {Component} from 'react';
 import { Row, Col } from 'antd';
+import axios from 'axios';
 import SatSetting from './SatSetting';
 import SatelliteList from './SatelliteList';
+import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constants";
 
 class Main extends Component {
+    constructor(){
+        super();
+        this.state = {
+            satInfo: null,
+            settings: null,
+            isLoadingList: false
+        };
+    }
+
+    showNearbySatellite = (setting) => {
+        this.setState({
+            settings: setting
+        })
+        this.fetchSatellite(setting);
+    }
+
+    fetchSatellite= (setting) => {
+        const {latitude, longitude, elevation, altitude} = setting;
+        const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+
+   this.setState({
+       isLoadingList: true
+   });
+
+   axios.get(url)
+       .then(response => {
+           console.log(response.data)
+           this.setState({
+               satInfo: response.data,
+               isLoadingList: false
+           })
+       })
+       .catch(error => {
+           console.log('err in fetch satellite -> ', error);
+       })
+}
+
     render() {
+        const { satInfo } = this.state;
         return (
-            <Row className='Main'>
+            <Row className='main'>
                 <Col span={8} className="left-side">
-                    <SatSetting />
-                    <SatelliteList />
+                    <SatSetting onShow={this.showNearbySatellite}/>
+                    <SatelliteList satInfo={satInfo}
+                                   isLoad={this.state.isLoadingList}
+                    />
                 </Col>
                 <Col span={16} className="right-side">
                     right
@@ -18,4 +61,7 @@ class Main extends Component {
         );
     }
 }
+
 export default Main;
+
+
